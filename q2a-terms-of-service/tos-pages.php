8805a3c0591d0c89c5aab0596bb50db7f1dff767
Towhidn <Towhidn@gmail.com>
2014-03-31 10:28:20 -0800

    small issue was fixed
    TOS would show up when plugin was disabled and ServerSide check was
    enabled.

--- q2a-terms-of-service/tos-pages.php
+++ q2a-terms-of-service/tos-pages.php
@@ -3,7 +3,7 @@
 	function qa_page_routing()
 	{
 		$pages = qa_page_routing_base();
-		if (qa_opt('tos_serverside'))
+		if( (qa_opt('tos_enable')) && (qa_opt('tos_serverside')) )
 			$pages['register'] = '../qa-plugin/q2a-terms-of-service/qa-page-register.php'; // changed to include a new file instead of default page
 		return $pages;
 	}
